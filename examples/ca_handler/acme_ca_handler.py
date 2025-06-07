ad16c7145cd4a1ad3cb3b70c8c16c00f28ce27b1
grindsa <grindelsack@gmail.com>
2025-06-07 07:56:11 +0200

    bump

--- examples/ca_handler/acme_ca_handler.py
+++ examples/ca_handler/acme_ca_handler.py
@@ -5,30 +5,34 @@ from __future__ import print_function
 # pylint: disable= e0401, w0105, w0212
 import json
 import textwrap
-import base64
 import os.path
 from typing import Tuple, Dict
 import requests
 import josepy
-from OpenSSL import crypto
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from acme import client, messages
 from acme import errors
 from acme_srv.db_handler import DBstore
 from acme_srv.helper import (
     load_config,
+    allowed_domainlist_check,
+    b64_decode,
+    b64_encode,
     b64_url_recode,
-    parse_url,
+    client_parameter_validate,
+    config_allowed_domainlist_load,
     config_eab_profile_load,
     config_headerinfo_load,
-    config_profile_load,
-    client_parameter_validate,
-    eab_profile_header_info_check,
     config_enroll_config_log_load,
+    config_profile_load,
+    eab_profile_header_info_check,
     enrollment_config_log,
-    config_allowed_domainlist_load,
-    allowed_domainlist_check,
+    parse_url,
+    cert_pem2der,
+    uts_now,
+    uts_to_date_utc,
 )
 
 
@@ -304,17 +308,18 @@ class CAhandler(object):
         try:
             if self.profile:
                 # profile is set
-                self.logger.debug("CAhandler._order_issue() adding profile: %s", self.profile)
-                order = acmeclient.new_order(
-                    csr_pem=csr_pem, profile=self.profile
+                self.logger.debug(
+                    "CAhandler._order_issue() adding profile: %s", self.profile
                 )
+                order = acmeclient.new_order(csr_pem=csr_pem, profile=self.profile)
             else:
                 # no profile set
                 self.logger.debug("CAhandler._order_issue() no profile set")
                 order = acmeclient.new_order(csr_pem=csr_pem)
         except Exception as err:
             self.logger.error(
-                "CAhandler._order_issue() failed to create order: %s. Try without profile information.", err
+                "CAhandler._order_issue() failed to create order: %s. Try without profile information.",
+                err,
             )
             order = acmeclient.new_order(csr_pem=csr_pem)
 
@@ -332,14 +337,11 @@ class CAhandler(object):
             if order.fullchain_pem:
                 self.logger.debug("CAhandler.enroll() successful")
                 cert_bundle = str(order.fullchain_pem)
-                cert_raw = str(
-                    base64.b64encode(
-                        crypto.dump_certificate(
-                            crypto.FILETYPE_ASN1,
-                            crypto.load_certificate(crypto.FILETYPE_PEM, cert_bundle),
-                        )
-                    ),
-                    "utf-8",
+                # Split the chain into individual certificates
+                certs = cert_bundle.strip().split("-----END CERTIFICATE-----")
+                # The first certificate is the end-entity certificate
+                cert_raw = b64_encode(
+                    self.logger, cert_pem2der(certs[0] + "-----END CERTIFICATE-----")
                 )
             else:
                 self.logger.error(
@@ -545,9 +547,7 @@ class CAhandler(object):
         )
 
         result = None
-        new_value, error = client_parameter_validate(
-            self.logger, csr, self, key, value
-        )
+        new_value, error = client_parameter_validate(self.logger, csr, self, key, value)
         if new_value:
             self.logger.debug(
                 "CAhandler._eab_profile_list_set(): setting attribute: %s to %s",
@@ -760,7 +760,10 @@ class CAhandler(object):
         return (error, cert_bundle, cert_raw, poll_identifier, rejected)
 
     def revoke(
-        self, _cert: str, _rev_reason: str, _rev_date: str
+        self,
+        _cert: str,
+        _rev_reason: str = "unspecified",
+        _rev_date: str = uts_to_date_utc(uts_now()),
     ) -> Tuple[int, str, str]:
         """revoke certificate"""
         self.logger.debug("CAhandler.revoke()")
@@ -771,11 +774,9 @@ class CAhandler(object):
         detail = None
 
         try:
-            certpem = f"-----BEGIN CERTIFICATE-----\n{textwrap.fill(str(b64_url_recode(self.logger, _cert)), 64)}\n-----END CERTIFICATE-----\n"
-            cert = josepy.ComparableX509(
-                crypto.load_certificate(crypto.FILETYPE_PEM, certpem)
+            cert = x509.load_der_x509_certificate(
+                b64_decode(self.logger, _cert), backend=default_backend()
             )
-
             if os.path.exists(self.acme_keyfile):
                 user_key = self._user_key_load()
             net = client.ClientNetwork(user_key)
